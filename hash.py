bbc4bc8763f2b615a4df39cb525fe831cea936a2
carlpadilla <carlpadilla@protonmail.com>
2020-07-07 23:29:59 -0400

    adding notes from lecture

--- hash.py
+++ hash.py
@@ -30,7 +30,7 @@ def my_hash(s):
 
 
 ​
-return total
+   return total
 ​
 # ASCII: assigns letters to numbers
 # ord()
@@ -46,15 +46,19 @@ def my_hash(s):
 
 
 ​
-total = 0
-for c in s_utf8:
-    total += c
+   total =
+       0
+    for c in s_utf8:
+        total += c
 ​
-return total
+   return total
+​
+​
+"Fourscore and seven years,/nshe sells seashells"
 ​
 hello_index = my_hash("hello")  # 532
 ​
-my_arr = [None] * 5
+my_arr = [None] * 500000
 ​
 hello_index = hello_index % len(my_arr)
 ​
@@ -89,8 +93,18 @@ print(my_arr[key_index])
 # 1. hash the key/word, get number back from hash function
 # 2. modulo with array length to find index
 # 3. look up value at that index, return it
+​
+# dynamic programming
+​
+# hash function ideal speeds
+# in a hash table: fast! we want to look stuff up FAST
+# to hash passwords and store hashes instead of plaintext passwords: relatively SLOW
+​
+# output of hash function: hash, or digest
+​
+# a good hash function like SHA256 gives a totally unique output
+# can use as id aka fingerprint of a document (document == big string)
+​
+# Can you reverse the output of a hash function? Can you go from 532 --> "hello"?
+# a good hash function is non-reversible
 Collapse
-
-
-: thankyou6:
-1
