eb932ef8e88c9f273606776bff46dbda84c9a71c
Carl Padilla <carlpadilla@protonmail.com>
2020-07-16 09:13:20 -0400

    completed

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,30 @@
-def word_count(s):
-    # Your code here
+ignore = (":", ";", ",", ".", "-", "+", "=", "/", "\\", "|",
+               "[", "]", "{", "}", "(", ")", "*", "^", "&", "\"")
 
+
+def word_count(s):
+    counts = {}
+
+    # convert to lower case
+    # split words into list
+    for char in ignore:
+        s = s.replace(char, "")
+    s = s.lower().split()
+
+    if len(s) == 1 and s[0] == "":
+        return {}
+
+    for word in s:
+        if word in counts:
+            counts[word.lower()] += 1
+        else:
+            counts[word.lower()] = 1
+    return counts
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
